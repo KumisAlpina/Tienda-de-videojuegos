849a9e883469c25c9d303c5c761154b472286060
KumisAlpina <alayonjulian@gamil.com>
2025-04-20 15:45:12 -0500

    se realizaron cambios en el orden de la tienda

--- tienda.py
+++ tienda.py
@@ -6,26 +6,31 @@ tienda = tienda('Tienda de videojuegos')
 while True:
     print('\n ============Tienda de videojuegos===========')
     print('1. Agregar producto')
-    print('2. Ver productos')
+    print('2. lista de productos')
     print('3. Vender producto')
     print('4. Salir')
 
     opcion = input('Seleccione una opción: ')
 
     if opcion == '1':
-        nombre = input('Ingrese el nombre del producto: ')
-        precio = float(input('Ingrese el precio del producto: '))
-        cantidad = int(input('Ingrese la cantidad del producto: '))
         tipo = input('Ingrese el tipo de producto (videojuego/consola): ').lower()
 
         if tipo == 'videojuego':
+            nombre = input('Ingrese el nombre del videojuego: ')
+            precio = float(input('Ingrese el precio del videojuego: '))
+            cantidad = int(input('Ingrese la cantidad del producto: '))
             genero = input('Ingrese el género del videojuego: ')
             plataforma = input('Ingrese la plataforma del videojuego: ')
             tienda.agregar_producto(Videojuego(nombre, precio, cantidad, genero, plataforma))
+
         elif tipo == 'consola':
+            nombre = input('Ingrese la marca de la consola: ')
+            precio = float(input('Ingrese el precio de la consola: '))
+            cantidad = int(input('Ingrese la cantidad de la consola: '))
             modelo = input('Ingrese el modelo de la consola: ')
             almacenamiento = int(input('Ingrese el almacenamiento de la consola (GB): '))
             tienda.agregar_producto(Consola(nombre, precio, cantidad, modelo, almacenamiento))
+
         else:
             print('Tipo de producto no válido.')
     
